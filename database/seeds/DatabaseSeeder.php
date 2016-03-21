81f56381b31e05f98fc61101eba7dbb31e1a707b
Dele O <delomos@gmail.com>
2016-03-20 19:37:56 -0500

    adding User Auth files

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UserTableSeeder');
+        DB::table('users')->insert([
+        'name' => str_random(10),
+        'email' => str_random(10).'@github.com',
+        'password' =>  Hash::make('secret'),
+    ]);
     }
 }
